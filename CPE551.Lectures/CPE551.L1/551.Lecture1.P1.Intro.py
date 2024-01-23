4e0696735631c6a937c764b912127773839256d5
Joris <jcwilson2021@gmail.com>
2024-01-23 16:13:13 -0500

    added folder for ICE with ICE 0; added ffolder for Lecture 1 with L1 notes

---
+++ CPE551.Lectures/CPE551.L1/551.Lecture1.P1.Intro.py
@@ -0,0 +1,20 @@
+'''
+Synthax = form of python (;, (), etc)
+Semantics = meaning of python (what does it do)
+
+Python is a high level language, meaning that it readible by humans 
+
+Machine language, or assembly language, is a low level language, meaning that it is readible by machines using 1s and 0s
+
+To translate high level language to low level language, we use and interpreter. An example of this is if we go the United Nations on a tour, and we enter a hall where there are people speaking different languages. We have an interpreter that translates what they are saying to us live with a set of headphones. This is the same concept as an interpreter in python, and does it line by line.
+
+On the other hand, an example of a compiler is if you wrote an entire book in enlglish, but other people want to read it but speak other languages. So when the book is finished, y9ou give the whole book to be translated. This is the same concept as a compiler in python, and does it all at once.
+
+Overall, python uses an interpreter. 
+
+An example of a compile ris a .exe file, which is a compiled file. If you try to open it, it will show you a bunch of 1s and 0s. When contorlling a device, you want to use a low level language to save time on the translation. This cannot be saved as a .py file.
+
+Treat this course as a way to play with python. To see if something works, open python, which will open the terminal, and I can try things one line at a time. This is interactive mode.
+
+Scrripting mode would be if I wrote a program in a file, and then ran it. This can be saved as a .py file.
+'''
